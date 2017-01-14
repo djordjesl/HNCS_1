773d0d781da1148502a0b02f59f41ad44e25856f
Nenad Pecanac <nenadgamer@live.com>
2017-01-14 18:16:04 +0100

    Source change

--- HNCS_1/Source.cpp
+++ HNCS_1/Source.cpp
@@ -9,6 +9,7 @@ int main()
 {
 	EnteringPassToll ept;
 
+
 	string searchUsername;
 	string searchPassword;
 	string location;
@@ -21,24 +22,28 @@ int main()
 	cin >> searchPassword;
 	cout << "Location: ";
 	cin >> location;
+
+	User user(searchUsername, searchPassword, location);
 	// if some other character is entered write a error
+
 	cout << "Entering pass toll (E) or Leaving pass tool (L): ";
 	cin >> position;
 
-	User user(searchUsername, searchPassword, location);
+
 	if (user.checkAuthorisation())
 	{
+
 		if (position == 'E')
 		{
 			cout << "For help on the road (H), Give ticket to user (G), Log out(0)";
 			cin >> position;
-			if (position == 'H')
+			while (position != '0')
 			{
-				ept.printHelp();
-			}
-			else if(position == 'G')
-			{
-				ept.printTicket(user);
+				if (position == 'H')
+					ept.printHelp();
+
+				else if (position == 'G')
+					ept.printTicket(user);
 			}
 		}
 	}
